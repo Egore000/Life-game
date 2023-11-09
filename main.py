8d148f416ff3e8960396ce01b7c231b70c5443af
Egore000 <egor.pakhomov.2002@gmail.com>
2023-11-09 20:04:53 +0700

    Реализовать жизненный цикл клеток. Вынести функции и классы в отдельный файл

--- main.py
+++ main.py
@@ -1,69 +1,23 @@
 import pygame
 import sys
 import time 
+import config as cfg
+from tools import Screen, Cell, Surface
 
 # инициализация pygame
 pygame.init()
 
-class Screen:
-    WIDTH = 640
-    HEIGHT = 480
-    COLOR = (0, 0, 0)
-
-class Cell:
-    SIZE = 10
-    COLOR = {
-        0: Screen.COLOR,
-        1: (255, 255, 255),
-    }
-    def __init__(self, x, y, status, cells):
-        self.x = (x // Cell.SIZE) * Cell.SIZE
-        self.y = (y // Cell.SIZE) * Cell.SIZE
-        self.status = status
-        self.color = Cell.COLOR[status]
-        self.body = pygame.Rect(self.x, self.y, Cell.SIZE, Cell.SIZE)
-        self.neighbours_coords = [
-            (self.x - Cell.SIZE, self.y + Cell.SIZE), 
-            (self.x - Cell.SIZE, self.y),
-            (self.x - Cell.SIZE, self.y - Cell.SIZE),
-            (self.x, self.y + Cell.SIZE),
-            (self.x, self.y - Cell.SIZE),
-            (self.x + Cell.SIZE, self.y + Cell.SIZE),
-            (self.x + Cell.SIZE, self.y),
-            (self.x + Cell.SIZE, self.y - Cell.SIZE)
-            ]
-        self.neighbours = self.count_neigbours(cells)
-
-    def count_neigbours(self, cells):
-        count = 0
-        for cell in cells.keys():
-            if cell in self.neighbours_coords and \
-                cells[cell].status == 1:
-                count += 1
-        return count
-
-def life(cells):
-    for cell in cells.values():
-        if cell.status == 0 and cell.neighbours == 3:
-            cell.status = 1
-            cell.color = Cell.COLOR[1]
-        elif cell.status and cell.neighbours in (2, 3):
-            pass
-        elif cell.status and (cell.neighbours not in (2, 3)):
-            cell.status = 0
-            cell.color = Cell.COLOR[0]
-    return cells
-
 screen = pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
 
 cells = {}
-for x in range(0, Screen.WIDTH, Cell.SIZE):
-    for y in range(0, Screen.HEIGHT, Cell.SIZE):
+for x in range(Surface.X_MIN, Surface.X_MAX, Cell.SIZE):
+    for y in range(Surface.Y_MIN, Surface.Y_MAX, Cell.SIZE):
         cells[(x, y)] = Cell(x, y, 0, cells)
 
 flag = True
 while True:
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
@@ -75,8 +29,9 @@ while True:
             x, y = pygame.mouse.get_pos()
             x = x // Cell.SIZE * Cell.SIZE
             y = y // Cell.SIZE * Cell.SIZE
-            cells[(x, y)].status = 1
-            cells[(x, y)].color = Cell.COLOR[1]
+            if Surface.X_MIN <= x <= Surface.X_MAX and Surface.Y_MIN <= y <= Surface.Y_MAX:
+                cells[(x, y)].status = 1
+                cells[(x, y)].color = Cell.COLOR[1]
         
         if event.type == pygame.MOUSEBUTTONUP:
             flag = False
@@ -86,18 +41,19 @@ while True:
             running = True
             while running:
                 count_alive = 0
-                cells = life(cells)
+                cells, cells_prev = Cell.life(cells)
+              
                 for cell in cells.values():
                     count_alive += cell.status
 
                     pygame.draw.rect(screen, cell.color, cell.body)
-                    pygame.display.update()
+                
+                time.sleep(1/cfg.FPS)
+                pygame.display.update()
 
                 if count_alive == 0:
+                    #  or cells == cells_prev:
                     running = False
-
-                # if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                #     running = False
 
     screen.fill(Screen.COLOR)     
     for cell in cells.values():
---
+++ tools.py
@@ -0,0 +1,55 @@
+import pygame
+import config as cfg 
+
+class Screen:
+    WIDTH = cfg.WIDTH
+    HEIGHT = cfg.HEIGHT
+    COLOR = cfg.BACKGROUNG_COLOR
+
+class Cell:
+    SIZE = cfg.CELL_SIZE
+    COLOR = cfg.CELL_COLOR
+    def __init__(self, x, y, status, cells):
+        self.x = (x // Cell.SIZE) * Cell.SIZE
+        self.y = (y // Cell.SIZE) * Cell.SIZE
+        self.status = status
+        self.color = Cell.COLOR[status]
+        self.body = pygame.Rect(self.x, self.y, Cell.SIZE, Cell.SIZE)
+
+    def get_neigbours(self, cells):
+        nb = []
+        for x in range(self.x - Cell.SIZE, self.x + 2*Cell.SIZE, Cell.SIZE):
+            for y in range(self.y - Cell.SIZE, self.y + 2*Cell.SIZE, Cell.SIZE):
+                try:
+                    if x == self.x and y == self.y:
+                        continue
+
+                    if Surface.X_MIN <= x <= Surface.X_MAX and \
+                        Surface.Y_MIN <= y <= Surface.Y_MAX and \
+                        cells[(x, y)].status == 1:
+                            nb.append(cells[(x, y)])
+                except:
+                    pass
+                
+        return nb
+    
+    def life(cells):
+        previous_condition = cells
+        for cell in cells.values():
+            neighbours = len(cell.get_neigbours(cells))
+            if cell.status == 0 and neighbours == 3:
+                cell.status = 1
+                cell.color = Cell.COLOR[1]
+            elif cell.status and neighbours in (2, 3):
+                pass
+            elif cell.status and (neighbours not in (2, 3)):
+                cell.status = 0
+                cell.color = Cell.COLOR[0]
+        return cells, previous_condition
+
+class Surface:
+    X_MIN = cfg.X_MIN
+    X_MAX = cfg.X_MAX
+    Y_MIN = cfg.Y_MIN
+    Y_MAX = cfg.Y_MAX
+
