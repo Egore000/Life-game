29331652fb69b2b6946346ae8b12d139221f06ee
Egore000 <egor.pakhomov.2002@gmail.com>
2023-11-09 12:28:16 +0700

    Добавить рисование клетками и жизненный цикл клеток

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time 
 
 # инициализация pygame
 pygame.init()
@@ -11,19 +12,17 @@ class Screen:
 
 class Cell:
     SIZE = 10
-    COLOR = (255, 255, 255)
-
+    COLOR = {
+        0: Screen.COLOR,
+        1: (255, 255, 255),
+    }
     def __init__(self, x, y, status, cells):
         self.x = (x // Cell.SIZE) * Cell.SIZE
         self.y = (y // Cell.SIZE) * Cell.SIZE
         self.status = status
-        self.neighbours = self.count_neigbours(cells)
+        self.color = Cell.COLOR[status]
         self.body = pygame.Rect(self.x, self.y, Cell.SIZE, Cell.SIZE)
-
-    def count_neigbours(self, cells):
-        # neighbours_x = (self.x - Cell.SIZE, self.x, self.x + Cell.SIZE)
-        # neighbours_y = (self.y - Cell.SIZE, self.y, self.y + Cell.SIZE)
-        neighbours_coords = [
+        self.neighbours_coords = [
             (self.x - Cell.SIZE, self.y + Cell.SIZE), 
             (self.x - Cell.SIZE, self.y),
             (self.x - Cell.SIZE, self.y - Cell.SIZE),
@@ -33,34 +32,75 @@ class Cell:
             (self.x + Cell.SIZE, self.y),
             (self.x + Cell.SIZE, self.y - Cell.SIZE)
             ]
+        self.neighbours = self.count_neigbours(cells)
+
+    def count_neigbours(self, cells):
         count = 0
-        for cell in cells:
-            if (cell.x, cell.y) in neighbours_coords:
+        for cell in cells.keys():
+            if cell in self.neighbours_coords and \
+                cells[cell].status == 1:
                 count += 1
         return count
 
-    def life():
-        return
+def life(cells):
+    for cell in cells.values():
+        if cell.status == 0 and cell.neighbours == 3:
+            cell.status = 1
+            cell.color = Cell.COLOR[1]
+        elif cell.status and cell.neighbours in (2, 3):
+            pass
+        elif cell.status and (cell.neighbours not in (2, 3)):
+            cell.status = 0
+            cell.color = Cell.COLOR[0]
+    return cells
 
 screen = pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
 
-running = True
-cells = []
-while running:
+cells = {}
+for x in range(0, Screen.WIDTH, Cell.SIZE):
+    for y in range(0, Screen.HEIGHT, Cell.SIZE):
+        cells[(x, y)] = Cell(x, y, 0, cells)
+
+flag = True
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            flag = True
+
+        if flag:
             x, y = pygame.mouse.get_pos()
-            cell = Cell(x, y, 1, cells)
-            cells.append(cell)
+            x = x // Cell.SIZE * Cell.SIZE
+            y = y // Cell.SIZE * Cell.SIZE
+            cells[(x, y)].status = 1
+            cells[(x, y)].color = Cell.COLOR[1]
+        
+        if event.type == pygame.MOUSEBUTTONUP:
+            flag = False
 
-        # if event.type == pygame.K_ESCAPE:
+            # print(cells[(x, y)].neighbours)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            running = True
+            while running:
+                count_alive = 0
+                cells = life(cells)
+                for cell in cells.values():
+                    count_alive += cell.status
 
+                    pygame.draw.rect(screen, cell.color, cell.body)
+                    pygame.display.update()
+
+                if count_alive == 0:
+                    running = False
+
+                # if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                #     running = False
 
     screen.fill(Screen.COLOR)     
-    for cell in cells:
-        pygame.draw.rect(screen, Cell.COLOR, cell.body, 0)
+    for cell in cells.values():
+        pygame.draw.rect(screen, cell.color, cell.body)
+    
     pygame.display.flip()
